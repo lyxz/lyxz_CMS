e8be54549803e68d75e1aea103819b6df7da8bdd
appleLu <luguosui0@163.com>
2015-02-27 17:32:06 +0800

    初始化

---
+++ src/main/java/Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("Hello world111!!!!!!!");
+		System.out.println("Hello world111!!!!!!!");
+		System.out.println("Hello world111!!!!!!!");
+		System.out.println("Hello world111!!!!!!!");
+		System.out.println("Hello world111!!!!!!!");
+		System.out.println("Hello world111!!!!!!!");
+		System.out.println("Hello world111!!!!!!!");
+		System.out.println("Hello world111!!!!!!!");
+		System.out.println("Hello world111!!!!!!!");
+	}
+}
